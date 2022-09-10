171241f3799c47a5e286cbf5ed536720928a6adb
Negar Saleh <ngrslh76@gmail.com>
2022-09-10 14:48:45 +0430

    website softweb3.ir

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$('#main_slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('#multi_slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:4
+        }
+    },
+    dots:true ,
+    autoplay:true,
+})
+
+$('#course_slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:4
+        }
+    },
+    dots:false ,
+    autoplay:false,
+})
